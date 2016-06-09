9ba6bb19e809f57bb5ffcbf00aa0c64e0022cb71
Fahad <fha423@gmail.com>
2016-06-09 14:53:55 -0400

    Add movie Loader image and fix margins

--- app/src/main/java/com/example/fahadhd/moviesearch/ViewMoviesFragment.java
+++ app/src/main/java/com/example/fahadhd/moviesearch/ViewMoviesFragment.java
@@ -1,10 +1,12 @@
 package com.example.fahadhd.moviesearch;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
 /**
@@ -21,5 +23,16 @@ public class ViewMoviesFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.view_movies_fragment, container, false);
         movieGrid = (GridView) rootView.findViewById(R.id.movieGrid);
         return rootView;
+    }
+    public class MovieAdapter extends ArrayAdapter{
+
+        public MovieAdapter(Context context, int resource) {
+            super(context, resource);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return super.getView(position, convertView, parent);
+        }
     }
 }
