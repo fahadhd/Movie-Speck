c16ca689c9bf83ae853c5d745994ca7b8187f274
Fahad <fha423@gmail.com>
2016-06-10 15:11:27 -0400

    Start support for tablets

--- app/src/main/java/com/example/fahadhd/moviesearch/MainActivity.java
+++ app/src/main/java/com/example/fahadhd/moviesearch/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static boolean TABLET = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/fahadhd/moviesearch/ViewMoviesFragment.java
+++ app/src/main/java/com/example/fahadhd/moviesearch/ViewMoviesFragment.java
@@ -1,19 +1,28 @@
 package com.example.fahadhd.moviesearch;
 
 import android.content.Context;
+import android.graphics.Point;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class ViewMoviesFragment extends Fragment {
     GridView movieGrid;
+    ArrayList<String> posters;
+    static int width;
+    boolean sortByPop;
+
     public ViewMoviesFragment() {
     }
 
@@ -21,7 +30,22 @@ public class ViewMoviesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.view_movies_fragment, container, false);
+
+        //Get the dimensions of the screen
+        WindowManager wm = (WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        //If device is a table show 6 items else show 3
+        if(MainActivity.TABLET){
+            width = size.x/6;
+        }
+        else{
+            width = size.x/3;
+        }
         movieGrid = (GridView) rootView.findViewById(R.id.movieGrid);
+        movieGrid.setColumnWidth(width);
         return rootView;
     }
     public class MovieAdapter extends ArrayAdapter{
